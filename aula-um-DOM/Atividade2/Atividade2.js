4f4ff3511f3fa321ee1e87802c513fefae7bdc3e
Geraldo Marques <108906049+GeraldoMarques@users.noreply.github.com>
2023-07-19 19:27:22 -0300

    Commit muito importante

---
+++ aula-um-DOM/Atividade2/Atividade2.js
@@ -0,0 +1,6 @@
+function minhaFuncao() {
+    var paragrafos = document.getElementsByTagName("p");
+    if (paragrafos.length > 0) {
+        paragrafos[0].innerHTML = "Olá, Seja bem-vindo ao mundo JS!";
+    }
+}
